5a59e6887a2d220c9d43a8c14ed515d396e92314
Wallace Vieira <108556424+Wallace027Dev@users.noreply.github.com>
2026-03-16 20:05:17 -0300

    refact: criando roteador do react e refatorando estilos

---
+++ src/styles/theme/dark.js
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+    palette: {
+        main: {
+            primary: "#21BFAF",
+            secondary: "#B0D9D5",
+            tertiary: "#D96F66",
+            white: "#F2F0EB",
+            black: "#0D0D0D",
+        },
+    },
+});
+
+export default theme;
